2b0b7b71fdde867d5ebc3456c5de2413867f6795
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-08-29 18:44:39 +0530

    done
    done

--- ASD-RG-AUGUST/product-server/server.js
+++ ASD-RG-AUGUST/product-server/server.js
@@ -1,31 +1,56 @@
 const http = require("http"); // http module import
-const url = require("url");
-const products = [
-    { id: 1, name: "Laptop", price: 67000 },
-    { id: 2, name: "Mobile", price: 670000 },
-    { id: 3, name: "Watch", price: 6700 },
-    { id: 4, name: "Tab", price: 670 },
 
-];
+const fs = require('fs');
+const path = require('path');
+const querystring = require("querystring");
 
+
+const products = [];
+console.log(products);
+
+//how to access static file in server
+function servefile(res,filepath,contentType) {
+    fs.readFile(filepath,(err,data)=>{
+        if(err){
+           res.writeHead(404,{'Content-Type':"text/plain"});
+           res.end("404 not found");
+        } else {
+             res.writeHead(200,{'Content-Type':contentType});
+             res.end(data);
+        }
+    })
+}
 //create server
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200,{'Content-Type':"application/json"});
-    //parse url
-    const x = url.parse(req.url,true).query;
-    //filter product
-    if(x.name) {
-        const searchname = x.name.toLowerCase();
-        const result = products.filter(p=>p.name.toLowerCase().includes(searchname));
-         res.end(JSON.stringify(result));
+    //show form
+   if(req.method === "GET" && req.url ==="/") {
+    servefile(res,path.join(__dirname,"public","index.html"),"text/html");
+
+   }
+   //insert form
+    else if (req.method === "POST" && req.url ==="/add"){
+        let body = "";
+        req.on("data",chunk =>{body += chunk;});
+        req.on("end",()=>{
+            const data = querystring.parse(body);
+            products.push({id:products.length +1, name:data.name,price:data.price});
+            let html = "<h2>all product</h2><ul>";
+            products.forEach(x => {
+                html += `<li>${x.id} - ${x.name}-${x.price}</li>`
+            });
+            html += "</ul><a href='/'>AddMore</a>";
+            res.writeHead(200,{'Content-Type':"text/html"});
+            res.end(html);
+        })
+
     }
-    else {
-         res.end(JSON.stringify(products));
-    }
+
+  
    
 });
 const port = 6700;
 server.listen(port, () => {
     console.log("server is running port 6700");
+
 });
